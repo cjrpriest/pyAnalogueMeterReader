f18d51ee1e0f7bb763c84b9ff7d1148297b0c863
Chris Priest <chris.priest@me.com>
2019-06-23 07:25:58 +0100

    oops, copy local config file to Config.py, not the same file name

--- deploy-and-run.sh
+++ deploy-and-run.sh
@@ -1,5 +1,5 @@
 # get latest code and force working copy to reflect latest version
 git reset --hard master
 git pull
-cp ../pyAnalogueMeterReader.Config.py ./
+cp ../pyAnalogueMeterReader.Config.py Config.py
 python Main.py
