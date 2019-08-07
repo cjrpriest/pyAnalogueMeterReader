8249b8a49013e8dc8f653dd4b84fdc496a542d91
Chris Priest <chris.priest@me.com>
2019-08-07 09:03:37 +0100

    updated config for local testing with a video file

--- Config.py
+++ Config.py
@@ -5,17 +5,16 @@ class Config:
     def __init__(self):
         pass
 
-    # frame_input = "picamera" # "picamera" to select the Raspberry Pi camera as the source
-    frame_input = "tests/20170524125832.avi"
+    frame_input = "tests/20170411122357.avi"
     generate_full_debug_frame = True
     image_rotation_degrees = -14
     frame_size = (320, 240)
-    # centre_of_dial = (199, 86)
     centre_of_dial = (199, 92)
-    red_threshold_lower_boundary = 10
-    red_threshold_upper_boundary = 170
+    red_threshold_lower_boundary = 170
+    red_threshold_upper_boundary = 10
     min_approx_poly_area = 400
     meter_measurement_interval_ms = 5000
     meter_max_historical_measurements = 1000
     average_rate_calculation_period_s = 90
     print_debug_info = False
+
